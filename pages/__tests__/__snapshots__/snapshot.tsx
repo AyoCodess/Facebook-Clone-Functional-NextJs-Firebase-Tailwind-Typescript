b259d113ccb77780fb26a1c8674db5247e29bf96
AyoCodess <ayo.daniel.adesanya@gmail.com>
2022-09-30 08:40:48 +0200

    working on tests

---
+++ pages/__tests__/__snapshots__/snapshot.tsx
@@ -0,0 +1,7 @@
+import { render } from '@testing-library/react';
+// import Index from '@/pages/index';
+
+// it('renders homepage unchanged', () => {
+//   render(<Index />);
+//   expect(Index).toMatchSnapshot();
+// });
